6344b6707d5f3eb82f19e570a4e17c5844f441b8
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2020-03-05 01:16:20 +0000

    multiplication

--- src/matFact.c
+++ src/matFact.c
@@ -4,7 +4,7 @@
 #define RAND01 ((double)random() / (double)RAND_MAX)
 #define DEBUG  1
 
-// GLOBAL VARIABLES
+/* global variables */
 double **A, **B, **L, **R;
 
 int parse_int(FILE *fp) {
@@ -34,8 +34,9 @@ double **new_matrix(int l, int c) {
 
   for (i = 0; i < l; i++) {
     matrix[i] = (double *) malloc(sizeof(double) * c);
-    for (j = 0; j < c; j++)
-      matrix[i][j] = 0;
+    for (j = 0; j < c; j++) {
+      matrix[i][j] = 0;  
+    }
   }
 
   return matrix;
@@ -61,8 +62,9 @@ void print_matrix(double **matrix, int l, int c) {
   int i, j;
 
   for (i = 0; i < l; i++) {
-    for (j= 0; j < c; j++)
+    for (j= 0; j < c; j++) {
       printf("%1.6lf ", matrix[i][j]);
+    }
     printf("\n");
   }
 }
@@ -80,6 +82,20 @@ void random_fill_LR(int nU, int nI, int nF) {
       R[i][j] = RAND01 / (double) nF;
 }
 
+void matrix_multiply(int nU, int nI, int nF) {
+  int i, j, k;
+
+  for (i = 0; i < nU; i++) {
+    for (j = 0; j < nI; j++) {
+      double sum = 0;
+      for (k = 0; k < nF; k++) {
+        sum += L[i][k] * R[k][j];
+      }
+      B[i][j] = sum;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   FILE *fp;
   char *fname;
@@ -89,56 +105,42 @@ int main(int argc, char **argv) {
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s <instance>\n", argv[0]);
-    exit(1);
+    return 1;
   }
 
   fname = argv[1];
   if (NULL == (fp = fopen(fname, "r"))) {
     fprintf(stderr, "Unable to open file: \'%s\'\n", fname);
-    exit(1);
+    return 1;
   }
 
-  iters = parse_int(fp);
-  alpha = parse_double(fp);
-  n_feats = parse_int(fp);
-  n_users = parse_int(fp); 
-  n_items = parse_int(fp);
-  nz_lines = parse_int(fp);
-  
-  if (DEBUG) {
-    printf("%d %1.6lf %d %d %d %d\n", iters, alpha, n_feats,
-        n_users, n_items, nz_lines);
-  }
+  iters = parse_int(fp);      /* iteration count */
+  alpha = parse_double(fp);   /* alpha constant */
+  n_feats = parse_int(fp);    /* number of latent features to consider */
+  n_users = parse_int(fp);    /* number of rows */
+  n_items = parse_int(fp);    /* number of columns */
+  nz_lines = parse_int(fp);   /* number of non-zero elements in A */
 
+  /* input matrix */
   A = new_matrix(n_users, n_items);
-  while (nz_lines--) {
-    int i = parse_int(fp), j = parse_int(fp);
-    A[i][j] = parse_double(fp);
-  }
-  
+  /* parse non-zero values into input matrix */
+  while (nz_lines--)
+    A[parse_int(fp)][parse_int(fp)] = parse_double(fp);
+
   if (0 != fclose(fp)) {
     fprintf(stderr, "Unable to flush file stream.\n");
-    exit(1);
-  }
-
-  if (DEBUG) {
-    printf("Initial matrix A\n");
-    print_matrix(A, n_users, n_items);
+    return 1;
   }
 
   L = new_matrix(n_users, n_feats);
   R = new_matrix(n_feats, n_items);
-  B = new_matrix(n_users, n_items);
   random_fill_LR(n_users, n_items, n_feats);
 
-  if (DEBUG) {
-    printf("Initial matrix L\n");
-    print_matrix(L, n_users, n_feats);
+  B = new_matrix(n_users, n_items);
+  matrix_multiply(n_users, n_items, n_feats);
+  print_matrix(B, n_users, n_items);
 
-    printf("Initial matrix R\n");
-    print_matrix(R, n_feats, n_items);
-  }
-
+  /* delete matrices */
   delete_matrix(B, n_users);
   delete_matrix(R, n_feats);
   delete_matrix(L, n_users);

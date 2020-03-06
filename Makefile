47158a27c05b8d37f1cedb0c817167cb87a1ace0
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2020-03-06 20:03:53 +0000

    factorization

--- Makefile
+++ Makefile
@@ -10,15 +10,15 @@ CFLAGS=-g -Wall
 
 
 $(TARGET): $(OBJECTS)
-	@echo " Linking..."
-	@echo " $(CC) $^ -o $(TARGET)"; $(CC) $^ -o $(TARGET)
+	@echo "Linking..."
+	@echo "$(CC) $^ -o $(TARGET)"; $(CC) $^ -o $(TARGET)
 
 $(BUILDDIR)/%.o: $(SRCDIR)/%.$(SRCEXT)
 	@mkdir -p $(BUILDDIR)
-	@echo " $(CC) $(CFLAGS) -c -o $@ $<"; $(CC) $(CFLAGS) -c -o $@ $<
+	@echo "$(CC) $(CFLAGS) -c -o $@ $<"; $(CC) $(CFLAGS) -c -o $@ $<
 
 clean:
-	@echo " Cleaning...";
-	@echo " $(RM) -r $(BUILDDIR) $(TARGET)"; $(RM) -r $(BUILDDIR) $(TARGET)
+	@echo "Cleaning...";
+	@echo "$(RM) -r $(BUILDDIR) $(TARGET)"; $(RM) -r $(BUILDDIR) $(TARGET)
 
 .PHONY: clean
--- src/matFact.c
+++ src/matFact.c
@@ -2,18 +2,11 @@
 #include <stdlib.h>
 
 #define RAND01 ((double)random() / (double)RAND_MAX)
-#define DEBUG  1
 
 // global variables ----------------------------------------
-struct matrix {
-  int l;
-  int c;
-  double **d;
-};
-
-struct matrix *A, *B;
-struct matrix *Lt, *L;
-struct matrix *Rt, *R;
+double **A, **B;
+double **Lt, **L;
+double **Rt, **R;
 
 // parsing helpers -----------------------------------------
 int parse_int(FILE *fp) {
@@ -35,53 +28,42 @@ double parse_double(FILE *fp) {
 }
 
 // matrix helpers ------------------------------------------
-struct matrix *new_matrix(int l, int c) {
+double **new_matrix(int l, int c) {
   int i, j;
 
-  struct matrix *m = (struct matrix *) malloc(sizeof(struct matrix));
-  m->l = l;
-  m->c = c;
-  m->d = (double **) malloc(sizeof(double *) * l);
-  if (NULL == m->d)
+  double **m = (double **) malloc(sizeof(double *) * l);
+  if (NULL == m)
     return NULL;
 
   for (i = 0; i < l; i++) {
-    m->d[i] = (double *) malloc(sizeof(double) * c);
+    m[i] = (double *) malloc(sizeof(double) * c);
     for (j = 0; j < c; j++) {
-      m->d[i][j] = 0;  
+      m[i][j] = 0;  
     }
   }
 
   return m;
 }
 
-void del_matrix(struct matrix *m) {
+void del_matrix(double **m, int l) {
   int i;
 
-  for (i = 0; i < m->l; i++)
-    free(m->d[i]);
-  free(m->d);
+  for (i = 0; i < l; i++)
+    free(m[i]);
   free(m);
+  return;
 }
 
-// needed?
-void cpy_matrix(struct matrix *from, struct matrix *to) {
+void print_matrix(double **m, int l, int c) {
   int i, j;
 
-  for (i = 0; i < from->l; i++)
-    for (j = 0; j < from->c; j++)
-      from->d[i][j] = to->d[i][j];
-}
-
-void print_matrix(struct matrix *m) {
-  int i, j;
-
-  for (i = 0; i < m->l; i++) {
-    for (j= 0; j < m->c; j++) {
-      printf("%1.6lf ", m->d[i][j]);
+  for (i = 0; i < l; i++) {
+    for (j= 0; j < c; j++) {
+      printf("%1.6lf ", m[i][j]);
     }
     printf("\n");
   }
+  return;
 }
 
 // matrix operations ---------------------------------------
@@ -92,141 +74,131 @@ void random_fill_LR(int nU, int nI, int nF) {
 
   for (i = 0; i < nU; i++)
     for (j = 0; j < nF; j++)
-      L->d[i][j] = RAND01 / (double) nF;
+      L[i][j] = RAND01 / (double) nF;
+
   for (i = 0; i < nF; i++)
     for (j = 0; j < nI; j++)
-      R->d[i][j] = RAND01 / (double) nF;
+      R[i][j] = RAND01 / (double) nF;
+  return;
 }
 
-void mult_matrix(int nU, int nI, int nF) {
+void matrix_mult_LR(int nU, int nI, int nF) {
   int i, j, k;
 
   for (i = 0; i < nU; i++) {
     for (j = 0; j < nI; j++) {
       double sum = 0;
       for (k = 0; k < nF; k++) {
-        sum += L->d[i][k] * R->d[k][j];
+        sum += L[i][k] * R[k][j];
       }
-      B->d[i][j] = sum;
+      B[i][j] = sum;
     }
   }
+  return;
 }
 
-void mat_fact(int iters, double a) {
+void matrix_fact_B(int nU, int nI, int nF, int n, double a) {
   int i, j, k;
-  struct matrix *tmp;
+  double **tmp;
 
-  while(iters-- <= 0) {
-    tmp = L;
-    L = Lt;
-    Lt = tmp;
+  while(--n) {
+    tmp = L; L = Lt; Lt = tmp;
+    tmp = R; R = Rt; Rt = tmp;
 
-    for (i = 0; i < L->l; i++) {
-      for (k = 0; k < L->c; k++) {
+    for (i = 0; i < nU; i++) {
+      for (k = 0; k < nF; k++) {
         double sum = 0;
-        for (j = 0; j < R->c; j++) {
-          if (A->d[i][j] == 0)
-            continue;
-          sum += 2*(A->d[i][j] - B->d[i][j])*(-R->d[k][j]);
+        for (j = 0; j < nI; j++) {
+          if (A[i][j])
+            sum += 2*(A[i][j] - B[i][j])*(-Rt[k][j]);
         }
-        L->d[i][k] = Lt->d[i][k] - (a * sum);
+        L[i][k] = Lt[i][k] - (a * sum);
       }
     }
 
-    tmp = R;
-    R = Rt;
-    Rt = tmp;
-
-    for (k = 0; k < R->l; k++) {
-      for (j = 0; j < R->c; j++) {
+    for (k = 0; k < nF; k++) {
+      for (j = 0; j < nI; j++) {
         double sum = 0;
-        for (i = 0; i < L->l; i++) {
-          if (A->d[i][j] == 0)
-            continue;
-
-          sum += 2*(A->d[i][j] - B->d[i][j])*(-L->d[i][k]);
+        for (i = 0; i < nU; i++) {
+          if (A[i][j])
+            sum += 2*(A[i][j] - B[i][j])*(-Lt[i][k]);
         }
-        R->d[k][j] = Rt->d[k][j] - (a * sum);
+        R[k][j] = Rt[k][j] - (a * sum);
       }
     }
 
-    for (i = 0; i < L->l; i++) {
-      for (j = 0; j < R->c; j++) {
-        double sum = 0;
-        for (k = 0; k < L->c; k++) {
-          sum += L->d[i][k] * R->d[k][j];
-        }
-        B->d[i][j] = sum;
-      }
-    }
+    matrix_mult_LR(nU, nI, nF);
   }
+  return;
+}
+
+void recommend(int l, int c) {
+  int i, j;
+
+  for (i = 0; i < l; i++) {
+    int item = 0;
+    double max = -1.00;
+    for (j = 0; j < c; j++) {
+      if ((B[i][j] > max) && !A[i][j]) {
+        max = B[i][j];
+        item = j;
+      }
+    }
+    printf("%d\n", item);
+  }
+  return;
 }
 
 // main ----------------------------------------------------
 int main(int argc, char **argv) {
   FILE *fp;
-  char *fname;
   double alpha;
-  int iters, lines;
-  int nUser, nItem, nFeat;
+  int N, lines, i, j;
+  int numU, numI, numF;
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s <instance>\n", argv[0]);
     return 1;
   }
 
-  fname = argv[1];
-  if (NULL == (fp = fopen(fname, "r"))) {
-    fprintf(stderr, "Unable to open file: \'%s\'\n", fname);
+  if ((fp = fopen(argv[1], "r")) == NULL) {
+    fprintf(stderr, "Unable to open file: \'%s\'\n", argv[1]);
     return 1;
   }
 
-  iters = parse_int(fp);      /* iteration count */
-  alpha = parse_double(fp);   /* alpha constant */
-  nFeat = parse_int(fp);      /* number of latent features to consider */
-  nUser = parse_int(fp);      /* number of rows */
-  nItem = parse_int(fp);      /* number of columns */
-  lines = parse_int(fp);      /* number of non-zero elements in A */
+  N = parse_int(fp);
+  alpha = parse_double(fp);
+  numF = parse_int(fp);
+  numU = parse_int(fp); numI = parse_int(fp); lines = parse_int(fp);
 
-  /* input matrix */
-  A = new_matrix(nUser, nItem);
+  A = new_matrix(numU, numI);
+  while (lines--) {
+    i = parse_int(fp);
+    j = parse_int(fp);
+    A[i][j] = parse_double(fp);
+  }
 
-  /* parse non-zero values into input matrix */
-  while (lines--)
-    A->d[parse_int(fp)][parse_int(fp)] = parse_double(fp);
-
-  if (0 != fclose(fp)) {
+  if (fclose(fp) != 0) {
     fprintf(stderr, "Unable to flush file stream.\n");
     return 1;
   }
 
-  /* user feats to consider */
-  L = new_matrix(nUser, nFeat);
-  Lt = new_matrix(nUser, nFeat);
+  L  = new_matrix(numU, numF);
+  Lt = new_matrix(numU, numF);
+  R  = new_matrix(numF, numI);
+  Rt = new_matrix(numF, numI);
+  B  = new_matrix(numU, numI);
 
-  /* feats present in the items */
-  R = new_matrix(nFeat, nItem);
-  Rt = new_matrix(nFeat, nItem);
+  random_fill_LR(numU, numI, numF);
+  matrix_mult_LR(numU, numI, numF);
+  matrix_fact_B(numU, numI, numF, N, alpha);
+  recommend(numU, numI);
 
-  /* init L and R */
-  random_fill_LR(nUser, nItem, nFeat);
-
-  /* initial matrix B */
-  B = new_matrix(nUser, nItem);
-
-  /* matrix factorization */
-  mult_matrix(nUser, nItem, nFeat);
-
-  mat_fact(iters, alpha);
-
-  print_matrix(B);
-
-  /* delete matrices */
-  del_matrix(B);
-  del_matrix(Rt);
-  del_matrix(R);
-  del_matrix(Lt);
-  del_matrix(L);
-  del_matrix(A);
+  del_matrix(B, numU);
+  del_matrix(Rt, numF); 
+  del_matrix(R, numF);
+  del_matrix(Lt, numU);
+  del_matrix(L, numU);
+  del_matrix(A, numU);
   return 0;
 }

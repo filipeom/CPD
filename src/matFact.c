7e244a506b63056afc80ad4a170f0bdfd107c91a
filipeom <33165733+filipeom@users.noreply.github.com>
2020-03-27 15:49:17 +0000

    Refactor and CSR fix (#14)
    * improve fact
    
    * add instances
    
    * names
    
    * csr fix
    
    * csr fix2
    
    Co-authored-by: Filipe Marques <filipe.s.o.marques@gmail.com>

--- src/matFact.c
+++ src/matFact.c
@@ -63,65 +63,63 @@ parse_double(FILE *fp)
 }
 
 /* matrix helpers */
-double*
-new_matrix(uint l, uint c)
+void
+matrix_init(double *matrix[], uint l, uint c)
 {
-  double *m = (double *) malloc(sizeof(double) * l * c);
-  if (!m)
+  *matrix = (double *) malloc(sizeof(double) * l * c);
+  if (NULL == *matrix)
     die("unable to allocate memory for matrix.\n");
 
-  memset(m, '\x00', sizeof(double)*l*c);
-
-  return m;
+  memset(*matrix, '\x00', sizeof(double) * l * c);
 }
 
 void
-delete_matrix(double *m)
+matrix_destroy(double *matrix)
 {
-  if (m)
-    free(m);
+  if (NULL != matrix)
+    free(matrix);
 }
 
 void
-print_matrix(const double *m, uint l, uint c)
+print_matrix(const double *matrix, uint l, uint c)
 {
   size_t i, j;
 
   for (i = 0; i < l; ++i) {
     for (j= 0; j < c; ++j) {
-      printf("%1.6lf ", m[i*c + j]);
+      printf("%1.6lf ", matrix[i*c + j]);
     }
     printf("\n");
   }
 }
 
-struct csr*
-new_csr_matrix(uint num_nz)
+void
+csr_matrix_init(struct csr **matrix, uint nnz, uint nU)
 {
-  struct csr *m;
-
-  m = (struct csr *) malloc(sizeof(struct csr));
-  if (!m)
+  *matrix = (struct csr *) malloc(sizeof(struct csr));
+  if (NULL == *matrix)
     die("unable to allocate memory for sparse matrix.\n");
 
-  m->row = (uint *) malloc(sizeof(uint) * num_nz);
-  m->col = (uint *) malloc(sizeof(uint) * num_nz);
-  m->val = (double *) malloc(sizeof(double) * num_nz);
+  (*matrix)->row = (uint *) malloc(sizeof(uint) * (nU + 1));
+  (*matrix)->col = (uint *) malloc(sizeof(uint) * nnz);
+  (*matrix)->val = (double *) malloc(sizeof(double) * nnz);
 
-  if (!m->row || !m->col || !m->val)
+  if (NULL == (*matrix)->row || NULL == (*matrix)->col ||
+      NULL == (*matrix)->val)
     die("unable to allocate memory for sparse matrix.\n");
 
-  return m;
+  memset((*matrix)->row, '\x00', sizeof(uint) * (nU + 1));
 }
 
 void
-delete_csr_matrix(struct csr *m)
+csr_matrix_destroy(struct csr *matrix)
 {
-  if (m->row && m->row && m->row && m) {
-    free(m->row);
-    free(m->col);
-    free(m->val);
-    free(m);
+  if (NULL != matrix->row && NULL != matrix->col &&
+      NULL != matrix->val && NULL != matrix) {
+    free(matrix->row);
+    free(matrix->col);
+    free(matrix->val);
+    free(matrix);
   }
 }
 
@@ -143,14 +141,39 @@ random_fill_LR(uint nU, uint nI, uint nF)
 }
 
 void
-matrix_mult_LR(uint nU, uint nI, uint nF)
+run(uint n, double alpha, uint nU, uint nI, uint nF)
 {
   size_t i, j, k;
+  size_t jx, item;
   double tmp;
   double *restrict m1;
   double *restrict m2;
   double *restrict mres;
 
+  /* matrix factorization */
+  for (size_t it = n; it--; ) {
+    memcpy(Rt, R, sizeof(double) * nI * nF);
+    memcpy(Lt, L, sizeof(double) * nU * nF);
+
+    for (i = 0; i < nU; ++i) {
+      for (jx = A->row[i]; jx < A->row[i+1]; ++jx) {
+        j = A->col[jx];
+        m1 = &L[i*nF];
+        m2 = &R[j*nF];
+        tmp = 0;
+        for (k = 0; k < nF; ++k) {
+          tmp += Lt[i*nF+k] * Rt[j*nF + k];
+        }
+        tmp = A->val[jx] - tmp;
+        for (k = 0; k < nF; ++k) {
+          m1[k] += alpha * 2 * tmp * Rt[j*nF + k];
+          m2[k] += alpha * 2 * tmp * Lt[i*nF + k];
+        }
+      }
+    }
+  }
+
+  /* matrix multiplication */
   for (i = 0, mres = &B[i*nI], m1 = &L[i*nF]; i < nU;
       ++i, mres += nI, m1 += nF) {
     for (j = 0, m2 = &R[j*nF]; j < nI; ++j, m2 += nF) {
@@ -161,52 +184,16 @@ matrix_mult_LR(uint nU, uint nI, uint nF)
       mres[j] = tmp;
     }
   }
-}
 
-void
-run(uint n, double alpha, uint nnz, uint nU, uint nI, uint nF)
-{
-  size_t i, j, k;
-  size_t ij, item;
-  double tmp, max;
-  double *restrict m1;
-  double *restrict m2;
-  double *restrict mres;
-
-  for (size_t it = n; it--; ) {
-    memcpy(Rt, R, sizeof(double) * nI*nF);
-    memcpy(Lt, L, sizeof(double) * nU*nF);
-
-    for (ij = 0; ij < nnz; ++ij) {
-      i = A->row[ij]; j = A->col[ij];
-      m1 = &L[i*nF];
-      m2 = &R[j*nF];
-      tmp = A->val[ij] - B[i*nI + j];
-      for (k = 0; k < nF; ++k) {
-        m1[k] += alpha * 2 * tmp * Rt[j*nF + k];
-        m2[k] += alpha * 2 * tmp * Lt[i*nF + k];
-      }
-    }
-
-    for (i = 0, mres = &B[i*nI], m1 = &L[i*nF]; i < nU;
-        ++i, mres += nI, m1 += nF) {
-      for (j = 0, m2 = &R[j*nF]; j < nI; ++j, m2 += nF) {
-        tmp = 0;
-        for (k = 0; k < nF; ++k) {
-          tmp += m1[k] * m2[k];
-        }
-        mres[j] = tmp;
-      }
-    }
-  }
-
-  for (i = 0; i < nnz; ++i)
-    B[A->row[i]*nI + A->col[i]] = 0;
+  /* FIXME: output */
+  for (i = 0; i < nU; ++i)
+    for (jx = A->row[i]; jx < A->row[i+1]; ++jx)
+      B[i*nI + A->col[jx]] = 0;
   for (i = 0; i < nU; ++i) {
-    item = 0; max = -1.0;
+    item = 0; tmp = -1.0;
     for (j = 0; j < nI; ++j) {
-      if (B[i*nI + j] > max) {
-        max = B[i*nI + j];
+      if (B[i*nI + j] > tmp) {
+        tmp = B[i*nI + j];
         item = j;
       }
     }
@@ -216,12 +203,13 @@ run(uint n, double alpha, uint nnz, uint nU, uint nI, uint nF)
 
 /* main */
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
   FILE *fp;
+  size_t i, ij;
 
   argv0 = argv[0];
-  if (argc != 2)
+  if (2 != argc)
     usage();
 
   if (NULL == (fp = fopen(argv[1], "r")))
@@ -234,33 +222,35 @@ main(int argc, char **argv)
   uint nI  = parse_uint(fp);
   uint nnz = parse_uint(fp);
 
-  A = new_csr_matrix(nnz);
-  for (size_t ix = 0; ix < nnz; ++ix) {
-    A->row[ix] = parse_uint(fp);
-    A->col[ix] = parse_uint(fp);
-    A->val[ix] = parse_double(fp);
+  csr_matrix_init(&A, nnz, nU);
+  for (ij = 0; ij < nnz; ++ij) {
+    i = parse_uint(fp);
+    A->row[i+1] += 1;
+    A->col[ij] = parse_uint(fp);
+    A->val[ij] = parse_double(fp);
   }
+  for (i = 1; i <= nU; ++i)
+    A->row[i] += A->row[i-1];
 
   if (0 != fclose(fp))
     die("unable to flush file stream.\n");
 
-  L  = new_matrix(nU, nF);
-  Lt = new_matrix(nU, nF);
-  R  = new_matrix(nI, nF);
-  Rt = new_matrix(nI, nF);
-  B  = new_matrix(nU, nI);
+  matrix_init(&L, nU, nF);
+  matrix_init(&Lt, nU, nF);
+  matrix_init(&R, nI, nF);
+  matrix_init(&Rt, nI, nF);
+  matrix_init(&B, nU, nI);
 
   random_fill_LR(nU, nI, nF);
-  matrix_mult_LR(nU, nI, nF);
 
-  run(N, a, nnz, nU, nI, nF);
+  run(N, a, nU, nI, nF);
 
-  delete_matrix(B);
-  delete_matrix(Rt);
-  delete_matrix(R);
-  delete_matrix(Lt);
-  delete_matrix(L);
-  delete_csr_matrix(A);
+  matrix_destroy(B);
+  matrix_destroy(Rt);
+  matrix_destroy(R);
+  matrix_destroy(Lt);
+  matrix_destroy(L);
+  csr_matrix_destroy(A);
 
   return 0;
 }

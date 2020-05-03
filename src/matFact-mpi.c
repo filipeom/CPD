a857935f36c64841688e038aeb198218946a36ea
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2020-05-03 14:03:23 +0100

    refactor

--- src/matFact-mpi.c
+++ src/matFact-mpi.c
@@ -5,40 +5,36 @@
 
 #include <mpi.h>
 
-#define RAND01 ((double)random() / (double)RAND_MAX)
+#define DEBUG  0
+#define RAND01 ((double)rand() / (double)RAND_MAX)
 
 typedef unsigned int uint;
 
-struct csr {
-  uint *row;
-  uint *col;
-  double *val;
-} __attribute__((aligned(64)));
-
 /* globals */
-static char *argv0 = NULL;
+static char *argv0 = NULL;  /* name of the binary */
 
-static int id = 0;
-static int nproc = 0;
+static int rank  = 0;  /* calling process id  */
+static int nproc = 0;  /* number of processes */
 
-static uint N   = 0;
-static double a = 0; 
-static uint nF  = 0;
-static uint nU  = 0;
-static uint nI  = 0;
+static uint N  = 0;  /* iteration count */
+static uint nF = 0;  /* number of feats */
+static uint nU = 0;  /* number of users */
+static uint nI = 0;  /* number of items */
 
-static double *A  = NULL;
-static double *Lt = NULL;
-static double *L  = NULL;
-static double *Rt = NULL;
-static double *R  = NULL;
+static double a = 0;  /* alpha constant */
+
+static double *A  = NULL;  /* evaluation matrix */
+static double *Lt = NULL;  /* users-feats matrix (prev it) */ 
+static double *L  = NULL;  /* users-feats matrix (curr it) */
+static double *Rt = NULL;  /* feats-items matrix (prev it) */
+static double *R  = NULL;  /* feats-items matrix (curr it) */
 
 /* general helpers */
 void
 die(const char *err_str, ...)
 {
   va_list ap;
-
+  
   va_start(ap, err_str);
   vfprintf(stderr, err_str, ap);
   va_end(ap);
@@ -48,30 +44,30 @@ die(const char *err_str, ...)
 void
 usage(void)
 {
-  die("usage: %s INSTANCE\n", argv0);
+  die("usage:\n\t%s <input-file>\n", argv0);
 }
 
 /* parsing helpers */
 uint
 parse_uint(FILE *fp)
 {
-  uint value;
+  uint val = 0;
 
-  if (1 != fscanf(fp, "%u", &value)) {
-    die("unable to parse integer.\n");
+  if (1 != fscanf(fp, "%u", &val)) {
+    die("[ERROR] %s: unable to parse value.\n", "parse_uint");
   }
-  return value;
+  return val;
 }
 
 double
 parse_double(FILE *fp)
 {
-  double value;
+  double val = 0;
 
-  if (1 != fscanf(fp, "%lf", &value)) {
-    die("unable to parse double.\n");
+  if (1 != fscanf(fp, "%lf", &val)) {
+    die("[ERROR] %s: unable to parse value.\n", "parse_double");
   }
-  return value;
+  return val;
 }
 
 /* matrix helpers */
@@ -80,81 +76,49 @@ matrix_init(double *matrix[], uint l, uint c)
 {
   *matrix = (double *) malloc(sizeof(double) * l * c);
   if (NULL == *matrix) {
-    die("unable to allocate memory for matrix.\n");
+    die("[ERROR] %s: unable to allocate matrix\n", "matrix_init");
   }
-
   memset(*matrix, '\x00', sizeof(double) * l * c);
 }
 
 void
-matrix_destroy(double *matrix)
-{
+matrix_destroy(double matrix[]) {
   if (NULL != matrix) {
     free(matrix);
   }
 }
 
 void
-print_matrix(const double *matrix, uint l, uint c)
+matrix_print(const double matrix[], const uint l, const uint c)
 {
   size_t i, j;
 
   for (i = 0; i < l; ++i) {
-    for (j= 0; j < c; ++j) {
+    for (j = 0; j < c; ++j) {
       printf("%1.6lf ", matrix[i * c + j]);
     }
     printf("\n");
   }
 }
 
-void
-csr_matrix_init(struct csr **matrix, uint nnz, uint nU)
-{
-  *matrix = (struct csr *) malloc(sizeof(struct csr));
-  if (NULL == *matrix) {
-    die("unable to allocate memory for sparse matrix.\n");
-  }
-
-  (*matrix)->row = (uint *) malloc(sizeof(uint) * (nU + 1));
-  (*matrix)->col = (uint *) malloc(sizeof(uint) * nnz);
-  (*matrix)->val = (double *) malloc(sizeof(double) * nnz);
-  if (NULL == (*matrix)->row || NULL == (*matrix)->col ||
-      NULL == (*matrix)->val) {
-    die("unable to allocate memory for sparse matrix.\n");
-  }
-
-  memset((*matrix)->row, '\x00', sizeof(uint) * (nU + 1));
-}
-
-void
-csr_matrix_destroy(struct csr *matrix)
-{
-  if (NULL != matrix->row && NULL != matrix->col &&
-      NULL != matrix->val && NULL != matrix) {
-    free(matrix->row);
-    free(matrix->col);
-    free(matrix->val);
-    free(matrix);
-  }
-}
-
-/* matrix operations */
+/* required initilizer */
 void
 random_fill_LR()
 {
   size_t i, j;
 
-  srandom(0);
+  srand(0);
 
   for (i = 0; i < nU; ++i) {
     for (j = 0; j < nF; ++j) {
       L[i * nF + j] = RAND01 / (double) nF;
-    }    
+    }
   }
+
   for (i = 0; i < nF; ++i) {
     for (j = 0; j < nI; ++j) {
       R[j * nF + i] = RAND01 / (double) nF;
-    }    
+    }
   }
 }
 
@@ -163,26 +127,20 @@ solve()
 {
   size_t i, j, k;
   double tmp, max;
-  double *restrict m1, *restrict mt1;
-  double *restrict m2, *restrict mt2;
+  double *m1, *mt1;
+  double *m2, *mt2;
   uint best[nU];
-
-  uint low = id * nU / nproc;
-  uint high = (id + 1) * nU / nproc;
-
-  for (size_t it = N; it--; ) {
-    memcpy(Rt, R, sizeof(double) * nI * nF);
+  
+  while (N--) {
     memcpy(Lt, L, sizeof(double) * nU * nF);
-    for (i = low, m1 = &L[i * nF], mt1 = &Lt[i * nF]; i < high; 
-        ++i, m1 += nF, mt1 += nF) {
+    memcpy(Rt, R, sizeof(double) * nI * nF);
+    for (i = 0; i < nU; ++i) {
+      m1 = &L[i * nF]; mt1 = &Lt[i * nF];
       for (j = 0; j < nI; ++j) {
-        if (A[i*nI + j]) {
-          m2 = &R[j * nF];
-          mt2 = &Rt[j * nF];
+        if (A[i*nI +j]) {
+          m2 = &R[j * nF]; mt2 = &Rt[j * nF];
           tmp = 0;
-          for (k = 0; k < nF; ++k) {
-            tmp += mt1[k] * mt2[k];
-          }
+          for (k = 0; k < nF; ++k) tmp += mt1[k] * mt2[k];
           tmp = a * 2 * (A[i*nI + j] - tmp);
           for (k = 0; k < nF; ++k) {
             m1[k] += tmp * mt2[k];
@@ -193,50 +151,48 @@ solve()
     }
   }
 
-  for (i = low, m1 = &L[i * nF]; i < high; ++i, m1 += nF) {
+  for (i = 0; i < nU; ++i) {
     max = 0;
-    for (j = 0, m2 = &R[j * nF]; j < nI; ++j, m2 += nF) {
-      if (!A[i*nI + j]) {
-        tmp = 0;
-        for (k = 0; k < nF; ++k) {
-          tmp += m1[k] * m2[k];
-        }
-        if (tmp > max) {
-          max = tmp;
-          best[i] = j;
-        }
-      } 
+    m1 = &L[i * nF];
+    for (j = 0; j < nI; ++j) {
+      m2 = &R[j * nF];
+      if (A[i*nI + j]) continue;
+      tmp = 0;
+      for (k = 0; k < nF; ++k) tmp += m1[k] * m2[k];
+      if (tmp > max) {
+        max = tmp;
+        best[i] = j;
+      }
     }
   }
 
-  for (i = low; i < high; ++i) {
-      printf("%u\n", best[i]);
-  }
+  if (0 == rank) for (i = 0; i < nU; ++i) printf("%u\n", best[i]);
 }
 
-/* main */
 int
-main(int argc, char *argv[])
+main(int argc, char* argv[])
 {
   MPI_Init(&argc, &argv);
 
-  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  if (0 == id) {
+  if (DEBUG) printf("[DEBUG] {rank=%d,nproc=%d}\n", rank, nproc);
+
+  if (0 == rank) {
     FILE *fp;
     size_t i, j, nnz;
 
-    argv0 = argv[0] ;
+    argv0 = argv[0];
     if (2 != argc) {
-      usage();
       MPI_Finalize();
-    }
-    
-    if (NULL == (fp = fopen(argv[1], "r"))) {
-      die("unable to open file: \'%s\'\n", argv[1]);
+      usage();
     }
 
+    if (NULL == (fp = fopen(argv[1], "r"))) {
+      die("[ERROR] %s: unable to open file \'%s\'\n", "main", argv[1]);
+    }
+    
     N   = parse_uint(fp);
     a   = parse_double(fp);
     nF  = parse_uint(fp);
@@ -246,35 +202,52 @@ main(int argc, char *argv[])
 
     matrix_init(&A, nU, nI);
 
-    for (size_t ij = 0; ij < nnz; ++ij) {
+    /**
+     * parse matrix A
+     * TODO: distribute matrix A
+     */
+    while (nnz--) {
       i = parse_uint(fp);
       j = parse_uint(fp);
-      A[i*nI + j] = parse_double(fp);
+      A[i* nI + j] = parse_double(fp);
     }
 
     if (0 != fclose(fp)) {
-      die("unable to flush file stream.\n");
+      die("[ERROR] %s: unable to flush file stream\n", "main");
     }
-  } 
+  }
 
-  // TODO: Send one vector of unsigned => 2.5x less comms
-  MPI_Bcast(&N, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  /* send parameters to nodes */
+  uint vec[4] = {N, nF, nU, nI};
+  MPI_Bcast(vec, 4, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
   MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&nF, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&nU, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-  MPI_Bcast(&nI, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+  N = vec[0]; nF = vec[1]; nU = vec[2]; nI = vec[3];
 
-  if (NULL == A)
-    matrix_init(&A, nU, nI);
-  
+  if (DEBUG) printf("[DEBUG] {rank=%d}->{N=%u,a=%lf,nF=%u,nU=%u,nI=%u}\n", rank, N, a, nF, nU, nI);
+
+  if (NULL == A) matrix_init(&A, nU, nI);
+
   MPI_Bcast(A, nU*nI, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+  
   matrix_init(&L, nU, nF);
   matrix_init(&Lt, nU, nF);
   matrix_init(&R, nI, nF);
   matrix_init(&Rt, nI, nF);
 
   random_fill_LR();
+  if (DEBUG) {
+    printf("[DEBUG] {rank=%d,L=\n", rank);
+    matrix_print(L, nU, nF);
+    printf("}\n");
+
+    printf("[DEBUG] {rank=%d,R=\n", rank);
+    matrix_print(R, nI, nF);
+    printf("}\n");
+
+    printf("[DEBUG] {rank=%d,A=\n", rank);
+    matrix_print(A, nU, nI);
+    printf("}\n");
+  }
   solve();
 
   matrix_destroy(Rt);
